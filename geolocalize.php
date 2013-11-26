7feb824073cf33822b1dd9086790c501ff1df692
Nes <carvantes@gmail.com>
2013-11-26 12:56:30 -0600

    direcciones bonitas

--- geolocalize.php
+++ geolocalize.php
@@ -1,31 +1,6 @@
-<?php
-// Include database connection settings
+<?php 
+
 include('config.inc');
-
-
-$miSentenciaSQL = "INSERT INTO Entrada (usuario, rol, origenx, origeny, destinox, destinoy,tiempo, lugares) VALUES ('".$_POST["username"]."','".$_POST["rol"]."','".$_POST["ox"]."','".$_POST["oy"]."','".$_POST["dx"]."','".$_POST["dy"]."','".$_POST["fecha"]." ".$_POST["hora"]."','".$_POST["lugares"]."')";
-
-if ($_POST["rol"] == "conductor"){
-	for ($i = 0; $i <$_POST["lugares"] ; $i++){
-		$resultado = mysql_query($miSentenciaSQL); 
-}
-	
-}else{
-	$resultado = mysql_query($miSentenciaSQL); 
-}
-
-	
-
-if (!$resultado){
- echo "Error al registrar la ruta"; exit;
- echo $miSentenciaSQL;
-}
-else
-{
-	//echo "Ruta insertada con &eacute;xito";
-} 
-
-$guardado_ok = true;
 
 $sql = "select * from entrada";
 $data = query($sql);
@@ -55,8 +30,4 @@ foreach ($data as $key => $val){
 	
 }
 
-include('bienvenido.php');
-
-              
 ?>
-
